4f346fae8ab267e25c0c318fe535cefcf8a836be
Andrew Wong <71823699+riezong@users.noreply.github.com>
2020-09-25 02:09:43 +1000

    Just Video

--- sketch.js
+++ sketch.js
@@ -22,6 +22,8 @@ var titleTime = 1000;
 var rotateTime = 4000;
 var creditsY = 576;
 
+var vid;
+
 // PFont mono;
 
 // PImage main;
@@ -67,238 +69,35 @@ var mnemonici;
 var mnemonicCount = 0;
 
 function setup() {
-  createCanvas(1920 / 2, 1080 / 2);
-  textSize(16);
-  textFont('Arial');
+    createCanvas(1920 / 2, 1080 / 2);
+    textSize(16);
+    textFont('Arial');
 
-  kuleshovi = int(random(kuleshovFrames));
-  showeri = int(random(showerFrames));
-  mnemonici = int(random(mnemonicFrames));
+    kuleshovi = int(random(kuleshovFrames));
+    showeri = int(random(showerFrames));
+    mnemonici = int(random(mnemonicFrames));
 
-  timer = millis() + titleTime;
+    timer = millis() + titleTime;
 
 }
 
 function draw() {
+    //Title Video
+    if (scene == 0) {
+        if (scene == 0) {
 
-  //Titles
-  if (scene == 1) {
-    background('#FF8800');
-    textAlign(CENTER, CENTER);
+            vid = createVideo(
+      ['data/Opening.mp4'],
+                vidLoad
+            );
 
-    text("Shower down memory drain", width / 2, height / 2 - 10);
-    text("Making sense of moments", width / 2, height / 2 + 10);
-
-    // Start countdown timer { 
-    countdown = ceil((timer - millis()) / 1000);
-    if (state == 0) {
-      text(countdown, 100, 100);
-    } else if (state == 1) {
-      text("score: 12", 100, 100);
-      scene = 15;
-      state = 0;
+            vid.size(width, height);
+        }
     }
-    if (timer < millis()) {
-      timer = millis() + titleTime;
-      state = 1;
-    }
-    // }
-
-  }
-
-  if (scene == 15) {
-    background('#000000');
-    textAlign(CENTER, CENTER);
-    fill(255);
-    text("Hypertexts: non-sequential writing", width / 2, height / 2 - 30);
-    text("Networked hypertexts do not have any designated beginning", width / 2, height / 2 + 10);
-    text("or any designated endings", width / 2, height / 2 + 30);
-
-    // Start countdown timer { 
-    countdown = ceil((timer - millis()) / 1000);
-    if (state == 0) {
-      text(countdown, 100, 100);
-    } else if (state == 1) {
-      text("score: 12", 100, 100);
-      scene = 2;
-      state = 0;
-    }
-    if (timer < millis()) {
-      timer = millis() + titleTime;
-      state = 1;
-    }
-    // }
-  }
-
-  //Title Video
-  if (scene == 0) {
-    
-    fingers = createVideo(['data/opening.mp4', ""]);
-    fingers.loop();
-//     for (var i = 0; i < N; i++) {
-
-//       var playFlag = (targetTime[i] - millis() > 0);
-
-//       if (playFlag) {
-//         image(myMovie[i], 0, 0, 1024, 576);
-//       }
-
-//       if (i == 0 && playFlag == false) {
-//         setMovie(0);
-//       }
-//     }
-
-    // Start countdown timer { 
-    countdown = ceil((timer - millis()) / 1000);
-    if (state == 0) {
-      text(countdown, 100, 100);
-    } else if (state == 1) {
-      text("score: 12", 100, 100);
-      scene = 2;
-      state = 0;
-    }
-    if (timer < millis()) {
-      timer = millis() + titleTime;
-      state = 1;
-    }
-    // }
-  }
-
-  //Main image
-  if (scene == 2) {
-    background(0);
-    main = createImg(Kuleshov[kuleshovi], "");
-    image(main, 0, 0, width, height);
-    main.hide();
-
-
-    // Start countdown timer { 
-    countdown = ceil((timer - millis()) / 1000);
-    if (state == 0) {
-      text(countdown, 100, 100);
-    } else if (state == 1) {
-      text("score: 12", 100, 100);
-      scene = 3;
-      state = 0;
-    }
-    if (timer < millis()) {
-      timer = millis() + rotateTime;
-
-      // //Generate new frame    
-      // var j = int(random(kuleshovFrames));
-      // //println(i, j);
-      // while (j == kuleshovi) {
-      //   j = int(random(kuleshovFrames));
-      //   //println("match");
-      // }
-      // kuleshovi = j;
-      state = 1;
-    }
-    // }
-
-  }
-
-  //Bathroom
-  if (scene == 3) {
-    //Loading image
-    img = createImg(shower[showeri], "");
-    image(img, 0, 0, width, height);
-    img.hide();
-
-    //Frame counter
-    //noStroke();
-    //fill(#000000);
-    //rect(5, 5, 90, 20);
-    //fill(#ff8800);
-    //text("frame " + (showeri+1), 10, 20);
-    //println(i);
-
-    //     // Calculate how much time has passed
-    //     var passedTime = millis() - savedTime;
-    //     // Has five seconds passed?
-    //     if (passedTime > rotateTime) {
-    //       savedTime = millis(); // Save the current time to restart the timer!
-    //       scene = 4;
-
-    //       //Generate new frame    
-    //       var j = int(random(showerFrames));
-    //       //println(i, j);
-    //       while (j == showeri) {
-    //         j = int(random(showerFrames));
-    //         //println("match");
-    //       }
-    //       showeri = j;
-    //     }
-  }
-
-  //Mnemonic/Memory
-  if (scene == 4) {
-    //Loading image
-    img = createImg(mnemonic[mnemonici]);
-    img.resize(1024, 576);
-    image(img, 0, 0);
-
-    //Frame counter
-    //noStroke();
-    //fill(#000000);
-    //rect(5, 5, 90, 20);
-    //fill(#ff8800);
-    //text("frame " + (mnemonici+1), 10, 20);
-    //println(i);
-
-    // Calculate how much time has passed
-    var passedTime = millis() - savedTime;
-    // Has five seconds passed?
-    if (passedTime > rotateTime) {
-      savedTime = millis(); // Save the current time to restart the timer!
-      scene = 2;
-
-      //Generate new frame    
-      var j = int(random(mnemonicFrames));
-      //println(i, j);
-      while (j == mnemonici) {
-        j = int(random(mnemonicFrames));
-        //println("match");
-      }
-      mnemonici = j;
-      mnemonicCount++;
-      if (mnemonicCount == mnemonicFrames) {
-        rotateTime = rotateTime / 3;
-      }
-      if (mnemonicCount == mnemonicFrames * 3) {
-        scene = 5;
-      }
-    }
-    //saveFrame("IS-Kuleshov-##.png");
-  }
-
-  //Credits
-  if (scene == 5) {
-    background('#000000');
-    fill(255);
-    textAlign(CENTER, CENTER);
-    text("Shower down memory drain", width / 2, creditsY);
-    text("Andrew Wong", width / 2, creditsY + 40);
-    text("2020", width / 2, creditsY + 80);
-    creditsY--;
-    //println(creditsY);
-  }
 }
 
-// Called every time a new frame is available to read
-// void movieEvent(Movie m) {
-//   m.read();
-// }
-
-// void setMovie(int idx) {
-//   myMovie[idx] = new Movie(this, getMovieName(idx));
-//   myMovie[idx].stop();
-//   myMovie[idx].play();
-//   myMovie[idx].volume(0);
-//   targetTime[idx] = millis() + myMovie[idx].duration() * 1000;
-// }
-
-// String getMovieName(int pos) {
-//   currIdx[pos]=(int)random(N);
-//   return names[currIdx[pos]];
-// }
+// This function is called when the video loads
+function vidLoad() {
+  vid.loop();
+  vid.volume(0);
+}
