c46e28a372fc5518ede9279232345c3f48a95eb3
Andrew Wong <71823699+riezong@users.noreply.github.com>
2020-09-25 01:49:00 +1000

    Stream of Thought v2

--- sketch.js
+++ sketch.js
@@ -15,48 +15,54 @@ var XPOS = [0, 260, 520];
 // SoundFile showerSound;
 
 var scene = 1;
-var savedTime;
-var titleTime = 10;
-var rotateTime = 40;
+var countdown;
+var state = 0;
+var timer;
+var titleTime = 1000;
+var rotateTime = 4000;
 var creditsY = 576;
 
 // PFont mono;
 
 // PImage main;
 var Kuleshov = [
-  "Tap 1.jpg",
-  "Tap 1.jpg",
-  "Tap 1.jpg",
-  "Tap 1.jpg",
-  "Tap 1.jpg",
-  "Tap 1.jpg",
-  "Tap 1.jpg",
-  "Tap 1.jpg",
-  "Tap 2.jpg",
-  "Tap 3.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Tap%201.jpg",
 ];
 
 // PImage img;
 var shower = [
-  "Shower 1.jpg",
-  "Shower 2.jpg",
-  "Shower 3.jpg",
-  "Shower 4.jpg",
-  "Shower 5.jpg",
-  "Shower 6.jpg"
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Shower%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Shower%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Shower%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Shower%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Shower%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Shower%201.jpg"
 ];
 var mnemonic = [
-  "Memory 1.jpg",
-  "Memory 2.jpg",
-  "Memory 3.jpg",
-  "Memory 4.jpg",
-  "Memory 5.jpg",
-  "Memory 6.jpg"
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Memory%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Memory%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Memory%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Memory%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Memory%201.jpg",
+  "https://raw.githubusercontent.com/riezong/streamofthought/master/data/Memory%201.jpg"
 ];
 
 var kuleshovFrames = 10;
 var showerFrames = 6;
 var mnemonicFrames = 6;
+
+var kuleshovi;
+var showeri;
+var mnemonici;
 
 var mnemonicCount = 0;
 
@@ -64,6 +70,13 @@ function setup() {
   createCanvas(1920 / 2, 1080 / 2);
   textSize(16);
   textFont('Arial');
+
+  kuleshovi = int(random(kuleshovFrames));
+  showeri = int(random(showerFrames));
+  mnemonici = int(random(mnemonicFrames));
+
+  timer = millis() + titleTime;
+
 }
 
 function draw() {
@@ -73,20 +86,24 @@ function draw() {
     background('#FF8800');
     textAlign(CENTER, CENTER);
 
-    var kuleshovi = int(random(kuleshovFrames));
-    var showeri = int(random(showerFrames));
-    var mnemonici = int(random(mnemonicFrames));
-
     text("Shower down memory drain", width / 2, height / 2 - 10);
     text("Making sense of moments", width / 2, height / 2 + 10);
 
-    // Countdown timer
-    if (frameCount % 60 == 0 && titleTime > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-      titleTime--;
-    }
-    if (titleTime == 0) {
+    // Start countdown timer { 
+    countdown = ceil((timer - millis()) / 1000);
+    if (state == 0) {
+      text(countdown, 100, 100);
+    } else if (state == 1) {
+      text("score: 12", 100, 100);
       scene = 15;
+      state = 0;
     }
+    if (timer < millis()) {
+      timer = millis() + titleTime;
+      state = 1;
+    }
+    // }
+
   }
 
   if (scene == 15) {
@@ -97,69 +114,96 @@ function draw() {
     text("Networked hypertexts do not have any designated beginning", width / 2, height / 2 + 10);
     text("or any designated endings", width / 2, height / 2 + 30);
 
-    // Countdown timer
-    if (frameCount % 60 == 0 && titleTime > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-      titleTime--;
-    }
-    if (titleTime == 0) {
+    // Start countdown timer { 
+    countdown = ceil((timer - millis()) / 1000);
+    if (state == 0) {
+      text(countdown, 100, 100);
+    } else if (state == 1) {
+      text("score: 12", 100, 100);
       scene = 2;
+      state = 0;
     }
+    if (timer < millis()) {
+      timer = millis() + titleTime;
+      state = 1;
+    }
+    // }
   }
 
   //Title Video
   if (scene == 0) {
-    for (var i = 0; i < N; i++) {
+    
+    fingers = createVideo(['data/opening.mp4', ""]);
+    fingers.loop();
+//     for (var i = 0; i < N; i++) {
 
-      var playFlag = (targetTime[i] - millis() > 0);
+//       var playFlag = (targetTime[i] - millis() > 0);
 
-      if (playFlag) {
-        image(myMovie[i], 0, 0, 1024, 576);
-      }
+//       if (playFlag) {
+//         image(myMovie[i], 0, 0, 1024, 576);
+//       }
 
-      if (i == 0 && playFlag == false) {
-        setMovie(0);
-      }
-    }
+//       if (i == 0 && playFlag == false) {
+//         setMovie(0);
+//       }
+//     }
 
-    // Countdown timer
-    if (frameCount % 60 == 0 && titleTime > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-      titleTime--;
-    }
-    if (titleTime == 0) {
+    // Start countdown timer { 
+    countdown = ceil((timer - millis()) / 1000);
+    if (state == 0) {
+      text(countdown, 100, 100);
+    } else if (state == 1) {
+      text("score: 12", 100, 100);
       scene = 2;
+      state = 0;
     }
+    if (timer < millis()) {
+      timer = millis() + titleTime;
+      state = 1;
+    }
+    // }
   }
 
   //Main image
   if (scene == 2) {
-    main = loadImage(Kuleshov[kuleshovi]);
-    main.resize(1024, 576);
-    image(main, 0, 0);
+    background(0);
+    main = createImg(Kuleshov[kuleshovi], "");
+    image(main, 0, 0, width, height);
+    main.hide();
 
-    // Countdown timer
-    if (frameCount % 60 == 0 && rotateTime > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-      rotateTime--;
-    }
-    if (rotateTime == 0) {
+
+    // Start countdown timer { 
+    countdown = ceil((timer - millis()) / 1000);
+    if (state == 0) {
+      text(countdown, 100, 100);
+    } else if (state == 1) {
+      text("score: 12", 100, 100);
       scene = 3;
-
-      //Generate new frame    
-      var j = int(random(kuleshovFrames));
-      //println(i, j);
-      while (j == kuleshovi) {
-        j = int(random(kuleshovFrames));
-        //println("match");
-      }
-      kuleshovi = j;
+      state = 0;
     }
+    if (timer < millis()) {
+      timer = millis() + rotateTime;
+
+      // //Generate new frame    
+      // var j = int(random(kuleshovFrames));
+      // //println(i, j);
+      // while (j == kuleshovi) {
+      //   j = int(random(kuleshovFrames));
+      //   //println("match");
+      // }
+      // kuleshovi = j;
+      state = 1;
+    }
+    // }
+
   }
 
   //Bathroom
   if (scene == 3) {
     //Loading image
-    img = loadImage(shower[showeri]);
-    img.resize(1024, 576);
-    image(img, 0, 0);
+    img = createImg(shower[showeri], "");
+    image(img, 0, 0, width, height);
+    img.hide();
 
     //Frame counter
     //noStroke();
@@ -169,29 +213,28 @@ function draw() {
     //text("frame " + (showeri+1), 10, 20);
     //println(i);
 
-    // Calculate how much time has passed
-    var passedTime = millis() - savedTime;
-    // Has five seconds passed?
-    if (passedTime > rotateTime) {
-      savedTime = millis(); // Save the current time to restart the timer!
-      scene = 4;
+    //     // Calculate how much time has passed
+    //     var passedTime = millis() - savedTime;
+    //     // Has five seconds passed?
+    //     if (passedTime > rotateTime) {
+    //       savedTime = millis(); // Save the current time to restart the timer!
+    //       scene = 4;
 
-      //Generate new frame    
-      var j = int(random(showerFrames));
-      //println(i, j);
-      while (j == showeri) {
-        j = int(random(showerFrames));
-        //println("match");
-      }
-      showeri = j;
-    }
-    //saveFrame("IS-Kuleshov-##.png");
+    //       //Generate new frame    
+    //       var j = int(random(showerFrames));
+    //       //println(i, j);
+    //       while (j == showeri) {
+    //         j = int(random(showerFrames));
+    //         //println("match");
+    //       }
+    //       showeri = j;
+    //     }
   }
 
   //Mnemonic/Memory
   if (scene == 4) {
     //Loading image
-    img = loadImage(mnemonic[mnemonici]);
+    img = createImg(mnemonic[mnemonici]);
     img.resize(1024, 576);
     image(img, 0, 0);
 
